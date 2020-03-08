7d5225d1f11d4e630ed70e4dd0e7cc717ea48d72
Alex Jose <turmawb@gmail.com>
2020-03-08 16:07:21 -0300

    DEV | tela documentos

--- capturardocumento.php
+++ capturardocumento.php
@@ -26,9 +26,10 @@
 	</div>
 	
 	<div id="corpocapturadocumento" class="corpo">
-
-		<label for="avatar">Escolha o arquivo:</label>
-		<input type="file"  id="documento" name="documento" accept="image/png, image/jpeg">
+		<?php
+			echo "SDFsfsd";
+			include "documento.php";
+		?>
 	</div>
 
 	<div id="rodape">
---
+++ documento.php
@@ -0,0 +1,4 @@
+<div>
+    <label for="avatar">Escolha o arquivo:</label>
+    <input type="file"  id="documento" name="documento" accept="image/png, image/jpeg">
+</div>
